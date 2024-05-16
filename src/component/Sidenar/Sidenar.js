188d6a9999b032ef0a5293acfe237b93edf47cbc
Tai Ngo <ndtt.2712@gmail.com>
2024-05-16 16:46:56 +0700

    Tai

--- src/component/Sidenar/Sidenar.js
+++ src/component/Sidenar/Sidenar.js
@@ -24,10 +24,10 @@ export const sidebartab = signal("Dashboard");
 export const sidebartabli = signal("none");
 const anamenu = signal(false);
 const setmenu = signal(false);
-const sys = signal([])
+const sys = signal([]);
 
 export default function Sidenar(props) {
-  const { isLandscape } = useMobileOrientation()
+  const { isLandscape } = useMobileOrientation();
   const dataLang = useIntl();
   const lang = useSelector((state) => state.admin.lang);
   const navigate = useNavigate();
@@ -37,7 +37,13 @@ export default function Sidenar(props) {
   const set_box = useRef();
 
   const data = {
-    Dashboard: { icon: <VscDashboard />, iconmobile: <ion-icon name="earth-outline" />, iconmobilefull: <ion-icon name="earth" />, link: "/", li: sys.value },
+    Dashboard: {
+      icon: <VscDashboard />,
+      iconmobile: <ion-icon name="earth-outline" />,
+      iconmobilefull: <ion-icon name="earth" />,
+      link: "/",
+      li: sys.value,
+    },
 
     // { link: "/Auto", name: dataLang.formatMessage({ id: "auto" }) },
 
@@ -55,7 +61,9 @@ export default function Sidenar(props) {
       ],
     },
     Analytics: {
-      icon: <TbReportAnalytics />, iconmobile: <ion-icon name="grid-outline" />, iconmobilefull: <ion-icon name="grid" />,
+      icon: <TbReportAnalytics />,
+      iconmobile: <ion-icon name="grid-outline" />,
+      iconmobilefull: <ion-icon name="grid" />,
       link: "none",
       li: [
         { link: "/Report", name: dataLang.formatMessage({ id: "report" }) },
@@ -63,7 +71,9 @@ export default function Sidenar(props) {
       ],
     },
     Setting: {
-      icon: <RiSettingsLine />, iconmobile: <ion-icon name="settings-outline" />, iconmobilefull: <ion-icon name="settings" />,
+      icon: <RiSettingsLine />,
+      iconmobile: <ion-icon name="settings-outline" />,
+      iconmobilefull: <ion-icon name="settings" />,
       link: "none",
       li:
         userInfor.value.type === "master"
@@ -189,13 +199,15 @@ export default function Sidenar(props) {
 
   const Menu = (id, label) => {
     return (
-      <div className="DAT_Sidenar_Content"
+      <div
+        className="DAT_Sidenar_Content"
         id={id}
         onClick={(event) => {
           handleMenu(event);
         }}
       >
-        <div className="DAT_Sidenar_Content-icon"
+        <div
+          className="DAT_Sidenar_Content-icon"
           style={{
             color:
               sidebartab.value === id
@@ -230,7 +242,8 @@ export default function Sidenar(props) {
             </label>
           </Link>
         )}
-        <div className="DAT_Sidenar_Content-arrow"
+        <div
+          className="DAT_Sidenar_Content-arrow"
           style={{ color: "rgb(141, 139, 139)" }}
         >
           {data[id].li.length === 0 ? (
@@ -300,40 +313,33 @@ export default function Sidenar(props) {
   }, []);
 
   useEffect(() => {
-    sys.value = []
+    sys.value = [];
     const which = {
       auto: "Auto",
       energy: "Energy",
       elev: "Elev",
-    }
+    };
 
-
-    const which_ = [
-      'energy',
-      'auto',
-      'elev',
-    ]
+    const which_ = ["energy", "auto", "elev"];
     Object.keys(ruleInfor.value.setting.system).map((key) => {
       if (ruleInfor.value.setting.system[key] === false) {
-        which_.splice(which_.indexOf(key), 1)
+        which_.splice(which_.indexOf(key), 1);
       }
-    })
-    console.log(which_)
+    });
+    console.log(which_);
     if (which_.length > 1) {
       which_.map((key) => {
         sys.value = [
           ...sys.value,
           { link: `/${which[key]}`, name: dataLang.formatMessage({ id: key }) },
-        ]
-      })
+        ];
+      });
     }
-
-  }, [ruleInfor.value, lang])
+  }, [ruleInfor.value, lang]);
 
   return (
     <>
-      {isBrowser || isLandscape
-        ?
+      {isBrowser || isLandscape ? (
         <>
           <div
             className="DAT_Sidenar"
@@ -395,106 +401,190 @@ export default function Sidenar(props) {
       ) : (
         <>
           <div className="DAT_SidenarMobile">
-            <div id="Dashboard" className="DAT_SidenarMobile_Item"
-              onClick={(e) => { handleMenu(e) }}
-              style={{ color: sidebartab.value === "Dashboard" ? dataColor.cur.color : dataColor.pre.color, }}
+            <div
+              id="Dashboard"
+              className="DAT_SidenarMobile_Item"
+              onClick={(e) => {
+                handleMenu(e);
+              }}
+              style={{
+                color:
+                  sidebartab.value === "Dashboard"
+                    ? dataColor.cur.color
+                    : dataColor.pre.color,
+              }}
             >
-              {data["Dashboard"].link === "none"
-                ?
+              {data["Dashboard"].link === "none" ? (
                 <div style={{ textAlign: "center" }}>
                   <div className="DAT_SidenarMobile_Item_Icon">
-                    {sidebartab.value === "Dashboard" ? data["Dashboard"].iconmobilefull : data["Dashboard"].iconmobile}
+                    {sidebartab.value === "Dashboard"
+                      ? data["Dashboard"].iconmobilefull
+                      : data["Dashboard"].iconmobile}
                   </div>
-                  <span>
-                    {dataLang.formatMessage({ id: "dashboard" })}
-                  </span>
+                  <span>{dataLang.formatMessage({ id: "dashboard" })}</span>
                 </div>
-                :
-                <Link to={data["Dashboard"].link}
+              ) : (
+                <Link
+                  to={data["Dashboard"].link}
                   style={{
                     textDecoration: "none",
-                    color: sidebartab.value === "Dashboard" ? dataColor.cur.color : dataColor.pre.color,
-                    textAlign: "center"
+                    color:
+                      sidebartab.value === "Dashboard"
+                        ? dataColor.cur.color
+                        : dataColor.pre.color,
+                    textAlign: "center",
                   }}
                 >
                   <div className="DAT_SidenarMobile_Item_Icon">
-                    {sidebartab.value === "Dashboard" ? data["Dashboard"].iconmobilefull : data["Dashboard"].iconmobile}
+                    {sidebartab.value === "Dashboard"
+                      ? data["Dashboard"].iconmobilefull
+                      : data["Dashboard"].iconmobile}
                   </div>
-                  <span>
-                    {dataLang.formatMessage({ id: "dashboard" })}
-                  </span>
+                  <span>{dataLang.formatMessage({ id: "dashboard" })}</span>
                 </Link>
-              }
+              )}
             </div>
 
-            <div id="Analytics" className="DAT_SidenarMobile_Item"
-              onClick={(e) => { handleMenu(e); anamenu.value = !anamenu.value }}
+            <div
+              id="Analytics"
+              className="DAT_SidenarMobile_Item"
+              onClick={(e) => {
+                handleMenu(e);
+                anamenu.value = !anamenu.value;
+              }}
               ref={ana_icon}
-              style={{ color: sidebartab.value === "Analytics" ? dataColor.cur.color : dataColor.pre.color, }}
+              style={{
+                color:
+                  sidebartab.value === "Analytics"
+                    ? dataColor.cur.color
+                    : dataColor.pre.color,
+              }}
             >
-              {data["Analytics"].link === "none"
-                ?
+              {data["Analytics"].link === "none" ? (
                 <div style={{ textAlign: "center" }}>
                   <div className="DAT_SidenarMobile_Item_Icon">
-                    {sidebartab.value === "Analytics" ? data["Analytics"].iconmobilefull : data["Analytics"].iconmobile}
+                    {sidebartab.value === "Analytics"
+                      ? data["Analytics"].iconmobilefull
+                      : data["Analytics"].iconmobile}
                   </div>
-                  <span>
-                    {dataLang.formatMessage({ id: "maintain" })}
-                  </span>
+                  <span>{dataLang.formatMessage({ id: "maintain" })}</span>
                 </div>
-                :
-                <Link to={data["Analytics"].link}
+              ) : (
+                <Link
+                  to={data["Analytics"].link}
                   style={{
                     textDecoration: "none",
-                    color: sidebartab.value === "Analytics" ? dataColor.cur.color : dataColor.pre.color,
-                    textAlign: "center"
+                    color:
+                      sidebartab.value === "Analytics"
+                        ? dataColor.cur.color
+                        : dataColor.pre.color,
+                    textAlign: "center",
                   }}
                 >
                   <div className="DAT_SidenarMobile_Item_Icon">
-                    {sidebartab.value === "Analytics" ? data["Analytics"].iconmobilefull : data["Analytics"].iconmobile}
+                    {sidebartab.value === "Analytics"
+                      ? data["Analytics"].iconmobilefull
+                      : data["Analytics"].iconmobile}
                   </div>
-                  <span>
-                    {dataLang.formatMessage({ id: "maintain" })}
-                  </span>
+                  <span>{dataLang.formatMessage({ id: "maintain" })}</span>
                 </Link>
-              }
+              )}
             </div>
 
-            <div id="Setting" className="DAT_SidenarMobile_Item"
+            <div
+              id="Setting"
+              className="DAT_SidenarMobile_Item"
               ref={set_icon}
-              onClick={(e) => { handleMenu(e); setmenu.value = !setmenu.value }}
-              style={{ color: sidebartab.value === "Setting" ? dataColor.cur.color : dataColor.pre.color, }}
+              onClick={(e) => {
+                handleMenu(e);
+                setmenu.value = !setmenu.value;
+              }}
+              style={{
+                color:
+                  sidebartab.value === "Setting"
+                    ? dataColor.cur.color
+                    : dataColor.pre.color,
+              }}
             >
-              {data["Setting"].link === "none"
-                ?
+              {data["Setting"].link === "none" ? (
                 <div style={{ textAlign: "center" }}>
                   <div className="DAT_SidenarMobile_Item_Icon">
-                    {sidebartab.value === "Setting" ? data["Setting"].iconmobilefull : data["Setting"].iconmobile}
+                    {sidebartab.value === "Setting"
+                      ? data["Setting"].iconmobilefull
+                      : data["Setting"].iconmobile}
                   </div>
-                  <span>
-                    {dataLang.formatMessage({ id: "setting" })}
-                  </span>
+                  <span>{dataLang.formatMessage({ id: "setting" })}</span>
                 </div>
-                :
-                <Link to={data["Setting"].link}
+              ) : (
+                <Link
+                  to={data["Setting"].link}
                   style={{
                     textDecoration: "none",
-                    color: sidebartab.value === "Setting" ? dataColor.cur.color : dataColor.pre.color,
-                    textAlign: "center"
+                    color:
+                      sidebartab.value === "Setting"
+                        ? dataColor.cur.color
+                        : dataColor.pre.color,
+                    textAlign: "center",
                   }}
                 >
                   <div className="DAT_SidenarMobile_Item_Icon">
-                    {sidebartab.value === "Setting" ? data["Setting"].iconmobilefull : data["Setting"].iconmobile}
+                    {sidebartab.value === "Setting"
+                      ? data["Setting"].iconmobilefull
+                      : data["Setting"].iconmobile}
                   </div>
-                  <span>
-                    {dataLang.formatMessage({ id: "setting" })}
-                  </span>
+                  <span>{dataLang.formatMessage({ id: "setting" })}</span>
                 </Link>
-              }
+              )}
             </div>
           </div>
+
+          <div
+            className="DAT_AnalyticsMenu"
+            style={{ display: anamenu.value ? "block" : "none" }}
+            ref={ana_box}
+          >
+            {data["Analytics"].li.map((data, index) => {
+              return data.link === "none" ? (
+                <span key={index}>{data.name}</span>
+              ) : (
+                <div
+                  className="DAT_AnalyticsMenu_Item"
+                  key={index}
+                  onClick={() => {
+                    handleDirect(data.link);
+                    anamenu.value = false;
+                  }}
+                >
+                  {data.name}
+                </div>
+              );
+            })}
+          </div>
+
+          <div
+            className="DAT_SettingMenu"
+            style={{ display: setmenu.value ? "block" : "none" }}
+            ref={set_box}
+          >
+            {data["Setting"].li.map((data, index) => {
+              return data.link === "none" ? (
+                <span key={index}>{data.name}</span>
+              ) : (
+                <div
+                  className="DAT_SettingMenu_Item"
+                  key={index}
+                  onClick={() => {
+                    handleDirect(data.link);
+                    setmenu.value = false;
+                  }}
+                >
+                  {data.name}
+                </div>
+              );
+            })}
+          </div>
         </>
-      }
+      )}
     </>
   );
 }
